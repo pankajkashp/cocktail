5afad3e55d0d3969cabbd8d9961e466b6c2cb2ad
pankajkashp26 <pankajkashap26@gmail.com>
2026-06-25 19:17:00 +0530

    art implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
+import Art from './components/Art'
  
 
 
@@ -16,6 +17,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About/>
+      <Art/>
     </main>
     
   )
